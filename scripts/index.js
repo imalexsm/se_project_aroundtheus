85aa4d6c7b64c04a25a2d6366e97d9a322422f31
Alex Santa Maria <alexsan@zillowgroup.com>
2023-04-25 22:52:23 -0700

    project 4 stage 4- profile title update and image template

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,54 @@ const initialCards = [
 }
 ]
 
+//Elements//
 const profileEditBtn = document.querySelector("#profile-edit-btn");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditCloseBtn = document.querySelector("#profile-edit-closebtn");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
-profileEditBtn.addEventListener('click', () =>{
-   profileEditModal.classList.add('modal_opened');
+//Functions//
+function closePopup() {
+    profileEditModal.classList.remove('modal_opened');
+}
+
+function getCardElement(cardData) {
+const cardElement = cardTemplate.cloneNode(true);
+const cardImageEl = cardElement.querySelector('.card__image');
+const cardTitleEl = cardElement.querySelector('.card__title');
+cardElement.querySelector('.card__image').src = cardData.link;
+cardElement.querySelector('.card__image').alt = cardData.name;
+cardTitleEl.textContent = cardData.name;
+return cardElement;
+}
+
+//Event Handlers//
+function handleProfileEditSubmit (e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+}
+
+//Event Listeners//
+profileEditBtn.addEventListener('click', () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileEditModal.classList.add('modal_opened');
+});
+
+profileEditCloseBtn.addEventListener('click', closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+ 
+initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardListEl.append(cardElement);
 })
 
-profileEditCloseBtn.addEventListener('click', () =>{
-    profileEditModal.classList.remove('modal_opened');
- })
